22e0a7ac3562806565d62d7d910910c50f74f20d
Harsh-Tripathi-231103 <harsh2454@gmail.com>
2025-12-09 11:51:31 +0530

    This program prints a square pattern of numbers from 1 to n for a given integer n.

---
+++ 1234sqpattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i, j,n;
+    cout<<"enter the number :";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            cout<<j;
+        }
+        cout<<endl;
+        }
+return 0;
+    }
+// This program prints a square pattern of numbers from 1 to n for a given integer n.
+// It uses nested loops to print each row and column of the pattern.
+// Example: If the input number is 4, the output will be:
+// 1234
+// 1234
+// 1234
+// 1234
+// The program utilizes basic input/output operations and loop constructs to achieve this functionality.
+
