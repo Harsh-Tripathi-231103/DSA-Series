7e42a70b1b50d0e9df16bda6adb744eb18ccaf4e
Harsh-Tripathi-231103 <harsh2454@gmail.com>
2025-12-09 12:25:32 +0530

    //     Ek simple function likho:
    // 👉 Function ka naam: printHelloDSA
    // 👉 Return type: void
    // 👉 Kaam: ek line print kare:
    
    // Hello DSA, let's crack product companies!

---
+++ basicfuncprogram.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void printHelloDSA(){
+    cout<<"Hello DSA, let's crack product companies!"<<endl;
+}
+int main(){
+printHelloDSA();
+}
+//     Ek simple function likho:
+
+// 👉 Function ka naam: printHelloDSA
+// 👉 Return type: void
+// 👉 Kaam: ek line print kare:
+
+// Hello DSA, let's crack product companies!
