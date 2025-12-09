4c82a6056d7e8fafc68e207f5cfa1a2ba7ce2f51
Harsh-Tripathi-231103 <harsh2454@gmail.com>
2025-12-09 10:51:29 +0530

    This program calculates the sum of the digits of a given integer.

---
+++ sumofdigits.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+    int sum=0;
+    while(n > 0){
+        int lastDigit = n % 10;
+        sum = sum + lastDigit;
+        n = n / 10;
+    }   
+    cout<<"Sum of digits is "<<sum<<endl;
+
+    return 0;
+}
