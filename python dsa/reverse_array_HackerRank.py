a0ceb9769e93462c542f3c93040037b262076e34
Harsh-Tripathi-231103 <harsh2454@gmail.com>
2026-02-27 08:55:56 +0530

    Hackerrank array problem:1 in python, solved with three different appraoches

---
+++ python dsa/reverse_array_HackerRank.py
@@ -0,0 +1,23 @@
+# first way of reversing an array
+arr = [1, 4, 3, 2]
+rev = arr[::-1]
+print(rev)   # [2, 3, 4, 1]
+
+# second way of reversing an array
+arr = [1, 4, 3, 2]
+arr.reverse()
+print(arr)   # [2, 3, 4, 1]
+
+# third way of reversing an array
+a = [1, 4, 3, 2]
+def reverseArray(a):
+    i = 0             # start index
+    j = len(a) - 1    # end index
+    
+    while i < j:
+        a[i], a[j] = a[j], a[i]  # swap
+        i += 1
+        j -= 1
+    
+    return a
+print(reverseArray(a))   # [2, 3, 4, 1]
